37f9b04de223317b64f5125b7fcfdb814b09be99
ktsu2i <kaito.tsu2i@gmail.com>
2025-01-15 21:17:47 +0900

    wrote test code for Add()

--- lists/linkedlist/linkedlist_test.go
+++ lists/linkedlist/linkedlist_test.go
@@ -10,5 +10,15 @@ func TestAdd(t *testing.T) {
 	ll.Add("A") // index: 0
 	ll.Add("B") // index: 1
 	ll.Add("C") // index: 2
-	ll.Add("D") // index: 3
+
+	if ll.Size() != 3 {
+		t.Errorf("Expected size 3, got %d", ll.Size())
+	}
+
+	expected := []string{"A", "B", "C"}
+	for i := 0; i < ll.Size(); i++ {
+		if val, _ := ll.Get(i); val != expected[i] {
+			t.Errorf("At index %d, expected %v, got %v", i, expected[i], val)
+		}
+	}
 }
