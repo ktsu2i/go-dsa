574ab2dcddb667e9abea4fc634756525242df3da
ktsu2i <kaito.tsu2i@gmail.com>
2025-01-18 17:52:39 +0900

    created Pop()

--- stack/stack.go
+++ stack/stack.go
@@ -11,3 +11,16 @@ func New[T any]() *Stack[T] {
 func (s *Stack[T]) Push(value T) {
 	s.data = append(s.data, value)
 }
+
+func (s *Stack[T]) Pop() (T, bool) {
+	if len(s.data) == 0 {
+		var value T
+		return value, false
+	}
+
+	// Get the top value
+	index := len(s.data) - 1
+	value := s.data[index]
+	s.data = s.data[:index]
+	return value, true
+}
