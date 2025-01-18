34bd89e0d4b0bb4c9a04365c73023db4b1e47b70
ktsu2i <kaito.tsu2i@gmail.com>
2025-01-18 16:13:32 +0900

    test code for Get()

--- lists/doublylinkedlist/doublylinkedlist_test.go
+++ lists/doublylinkedlist/doublylinkedlist_test.go
@@ -22,3 +22,50 @@ func TestAdd(t *testing.T) {
 		}
 	}
 }
+
+func TestGet(t *testing.T) {
+	dll := doublylinkedlist.New[string]()
+
+	// 1. Get from an empty list
+	_, err := dll.Get(0)
+	if err == nil {
+		t.Errorf("Expected error when getting from an empty list, but got nil")
+	}
+
+	dll.Add("A")
+	dll.Add("B")
+	dll.Add("C")
+
+	// 2. Get head
+	head, err := dll.Get(0)
+	if err != nil {
+		t.Errorf("Unexpected error for Get(0): %v", err)
+	}
+	if head != "A" {
+		t.Errorf("Expected head to be A, but got %s", head)
+	}
+
+	// 3. Get value at index 1
+	val, err := dll.Get(1)
+	if err != nil {
+		t.Errorf("Unexpected error for Get(1): %v", err)
+	}
+	if val != "B" {
+		t.Errorf("Expected B at index 1, but got %s", val)
+	}
+
+	// 4. Get tail
+	tail, err := dll.Get(2)
+	if err != nil {
+		t.Errorf("Unexpected error for Get(2): %v", err)
+	}
+	if tail != "C" {
+		t.Errorf("Expected tail to be C, but got %s", tail)
+	}
+
+	// 5. Get value at index out of bound
+	_, err = dll.Get(9999)
+	if err == nil {
+		t.Errorf("Expected error for index out of bound, but got nil")
+	}
+}
