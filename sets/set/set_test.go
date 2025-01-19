db396a0a07216a969273ebbb506b20abb91abe5b
ktsu2i <kaito.tsu2i@gmail.com>
2025-01-19 17:44:43 +0900

    test for Remove()

--- sets/set/set_test.go
+++ sets/set/set_test.go
@@ -35,3 +35,35 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Expected to have 1")
 	}
 }
+
+func TestRemove(t *testing.T) {
+	s := set.New[int]()
+
+	// 1. Empty set
+	s.Remove(10)
+	if s.Size() != 0 {
+		t.Errorf("Expected size 0, but got %d", s.Size())
+	}
+
+	s.Add(1)
+	s.Add(2)
+	s.Add(3)
+
+	// 2. Remove one value
+	s.Remove(1)
+	if s.Size() != 2 {
+		t.Errorf("Expected size 2, but got %d", s.Size())
+	}
+	if s.Contains(1) || !s.Contains(2) || !s.Contains(3) {
+		t.Errorf("Expected to only have 2 & 3")
+	}
+
+	// 3. Remove number that doesn't contain
+	s.Remove(9999)
+	if s.Size() != 2 {
+		t.Errorf("Expected size 2, but got %d", s.Size())
+	}
+	if s.Contains(9999) {
+		t.Errorf("Expected not to have 9999")
+	}
+}
