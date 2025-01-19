e6525254d42f32affe227a83b728c56639c56a48
Kaito Tsutsui <101069375+ktsu2i@users.noreply.github.com>
2025-01-19 14:01:37 +0900

    Binary Search Tree (#13)

---
+++ trees/bst/bst.go
@@ -0,0 +1,108 @@
+package bst
+
+import "golang.org/x/exp/constraints"
+
+type node[T constraints.Ordered] struct {
+	value T
+	left  *node[T]
+	right *node[T]
+}
+
+type BinarySearchTree[T constraints.Ordered] struct {
+	root *node[T]
+}
+
+func New[T constraints.Ordered]() *BinarySearchTree[T] {
+	return &BinarySearchTree[T]{}
+}
+
+func (bst *BinarySearchTree[T]) Insert(v T) {
+	if bst.root == nil {
+		bst.root = &node[T]{value: v}
+	} else {
+		insertNode(bst.root, v)
+	}
+}
+
+func (bst *BinarySearchTree[T]) Contains(v T) bool {
+	return containsNode(bst.root, v)
+}
+
+func (bst *BinarySearchTree[T]) PreOrder() []T {
+	var result []T
+	preOrderTraversal(bst.root, &result)
+	return result
+}
+
+func (bst *BinarySearchTree[T]) InOrder() []T {
+	var result []T
+	inOrderTraversal(bst.root, &result)
+	return result
+}
+
+func (bst *BinarySearchTree[T]) PostOrder() []T {
+	var result []T
+	postOrderTraversal(bst.root, &result)
+	return result
+}
+
+// Helper functions
+
+func insertNode[T constraints.Ordered](n *node[T], v T) {
+	if v < n.value {
+		// left
+		if n.left == nil {
+			n.left = &node[T]{value: v}
+		} else {
+			insertNode(n.left, v)
+		}
+	} else {
+		// right
+		if n.right == nil {
+			n.right = &node[T]{value: v}
+		} else {
+			insertNode(n.right, v)
+		}
+	}
+}
+
+func containsNode[T constraints.Ordered](n *node[T], v T) bool {
+	if n == nil {
+		return false
+	}
+
+	if v < n.value {
+		return containsNode(n.left, v)
+	} else if v > n.value {
+		return containsNode(n.right, v)
+	} else {
+		return true
+	}
+}
+
+func preOrderTraversal[T constraints.Ordered](n *node[T], result *[]T) {
+	if n == nil {
+		return
+	}
+	*result = append(*result, n.value)
+	preOrderTraversal(n.left, result)
+	preOrderTraversal(n.right, result)
+}
+
+func inOrderTraversal[T constraints.Ordered](n *node[T], result *[]T) {
+	if n == nil {
+		return
+	}
+	inOrderTraversal(n.left, result)
+	*result = append(*result, n.value)
+	inOrderTraversal(n.right, result)
+}
+
+func postOrderTraversal[T constraints.Ordered](n *node[T], result *[]T) {
+	if n == nil {
+		return
+	}
+	postOrderTraversal(n.left, result)
+	postOrderTraversal(n.right, result)
+	*result = append(*result, n.value)
+}
---
+++ trees/bst/bst_test.go
@@ -0,0 +1,214 @@
+package bst_test
+
+import (
+	"go-dsa/trees/bst"
+	"testing"
+)
+
+func TestInsert(t *testing.T) {
+	bst := bst.New[int]()
+
+	// 1. Insert 1 value
+	bst.Insert(10)
+	inorder := bst.InOrder()
+	expected := []int{10}
+	if len(inorder) != len(expected) || inorder[0] != 10 {
+		t.Errorf("Expected %v, but got %v", expected, inorder)
+	}
+
+	bst.Insert(5)
+	bst.Insert(15)
+	bst.Insert(2)
+	bst.Insert(8)
+	bst.Insert(12)
+	bst.Insert(20)
+
+	// 2. Insert 7 values
+	inorder = bst.InOrder()
+	expected = []int{2, 5, 8, 10, 12, 15, 20}
+	if len(inorder) != len(expected) {
+		t.Errorf("Length mismatch, expected %v, but got %v", expected, inorder)
+	}
+	for i, v := range expected {
+		if inorder[i] != v {
+			t.Errorf("Expected %d at index %d, but got %d", v, i, inorder[i])
+		}
+	}
+
+	// 3. Insert a duplicated number
+	bst.Insert(10)
+	inorder = bst.InOrder()
+	expected = []int{2, 5, 8, 10, 10, 12, 15, 20}
+	if len(inorder) != len(expected) {
+		t.Errorf("Length mismatch, expected %v, but got %v", expected, inorder)
+	}
+	for i, v := range expected {
+		if inorder[i] != v {
+			t.Errorf("Expected %d at index %d, but got %d", v, i, inorder[i])
+		}
+	}
+}
+
+func TestContains(t *testing.T) {
+	bst := bst.New[int]()
+
+	// 1. Empty BST
+	if bst.Contains(10) {
+		t.Errorf("Expected false checking if BST contains 10 on an empty BST, but got true")
+	}
+
+	values := []int{10, 5, 15, 2, 8, 12, 20}
+	for _, v := range values {
+		bst.Insert(v)
+	}
+
+	// 2. BST with 7 values
+	for _, v := range values {
+		if !bst.Contains(v) {
+			t.Errorf("Expected to contain %d, but got true", v)
+		}
+	}
+
+	// 3. Does NOT contain
+	notInTree := []int{0, 6, 25, 100}
+	for _, v := range notInTree {
+		if bst.Contains(v) {
+			t.Errorf("Expected not to contain %d, but got true", v)
+		}
+	}
+
+	// 4. Duplicated value
+	bst.Insert(10)
+	if !bst.Contains(10) {
+		t.Errorf("Expected to contain 10 after inserting the same value, but got false")
+	}
+}
+
+func TestPreOrder(t *testing.T) {
+	bst := bst.New[int]()
+
+	// 1. Empty BST
+	result := bst.PreOrder()
+	if len(result) != 0 {
+		t.Errorf("Expected empty PreOrder for an empty tree, but got %v", result)
+	}
+
+	bst.Insert(10)
+	bst.Insert(5)
+	bst.Insert(15)
+	bst.Insert(2)
+	bst.Insert(8)
+	bst.Insert(12)
+	bst.Insert(20)
+
+	// 2. BST with 7 values
+	result = bst.PreOrder()
+	expected := []int{10, 5, 2, 8, 15, 12, 20}
+	if len(result) != len(expected) {
+		t.Errorf("Length mismatch, expected %v, but got %v", expected, result)
+	}
+	for i, v := range expected {
+		if result[i] != v {
+			t.Errorf("Expected %d at index %d, but got %d", v, i, result[i])
+		}
+	}
+
+	// 3. Duplicated value
+	bst.Insert(10)
+	result = bst.PreOrder()
+	expected = []int{10, 5, 2, 8, 15, 12, 10, 20}
+	if len(result) != len(expected) {
+		t.Errorf("Length mismatch, expected %v, but got %v", expected, result)
+	}
+	for i, v := range expected {
+		if result[i] != v {
+			t.Errorf("Expected %d at index %d, but got %d", v, i, result[i])
+		}
+	}
+}
+
+func TestInOrder(t *testing.T) {
+	bst := bst.New[int]()
+
+	// 1. Empty BST
+	result := bst.InOrder()
+	if len(result) != 0 {
+		t.Errorf("Expected empty InOrder for an empty tree, but got %v", result)
+	}
+
+	bst.Insert(10)
+	bst.Insert(5)
+	bst.Insert(15)
+	bst.Insert(2)
+	bst.Insert(8)
+	bst.Insert(12)
+	bst.Insert(20)
+
+	// 2. BST with 7 values
+	result = bst.InOrder()
+	expected := []int{2, 5, 8, 10, 12, 15, 20}
+	if len(result) != len(expected) {
+		t.Errorf("Length mismatch, expected %v, but got %v", expected, result)
+	}
+	for i, v := range expected {
+		if result[i] != v {
+			t.Errorf("Expected %d at index %d, but got %d", v, i, result[i])
+		}
+	}
+
+	// 3. Duplicated value
+	bst.Insert(10)
+	result = bst.InOrder()
+	expected = []int{2, 5, 8, 10, 10, 12, 15, 20}
+	if len(result) != len(expected) {
+		t.Errorf("Length mismatch, expected %v, but got %v", expected, result)
+	}
+	for i, v := range expected {
+		if result[i] != v {
+			t.Errorf("Expected %d at index %d, but got %d", v, i, result[i])
+		}
+	}
+}
+
+func TestPostOrder(t *testing.T) {
+	bst := bst.New[int]()
+
+	// 1. Empty BST
+	result := bst.PostOrder()
+	if len(result) != 0 {
+		t.Errorf("Expected empty InOrder for an empty tree, but got %v", result)
+	}
+
+	bst.Insert(10)
+	bst.Insert(5)
+	bst.Insert(15)
+	bst.Insert(2)
+	bst.Insert(8)
+	bst.Insert(12)
+	bst.Insert(20)
+
+	// 2. BST with 7 values
+	result = bst.PostOrder()
+	expected := []int{2, 8, 5, 12, 20, 15, 10}
+	if len(result) != len(expected) {
+		t.Errorf("Length mismatch, expected %v, but got %v", expected, result)
+	}
+	for i, v := range expected {
+		if result[i] != v {
+			t.Errorf("Expected %d at index %d, but got %d", v, i, result[i])
+		}
+	}
+
+	// 3. Duplicated value
+	bst.Insert(10)
+	result = bst.PostOrder()
+	expected = []int{2, 8, 5, 10, 12, 20, 15, 10}
+	if len(result) != len(expected) {
+		t.Errorf("Length mismatch, expected %v, but got %v", expected, result)
+	}
+	for i, v := range expected {
+		if result[i] != v {
+			t.Errorf("Expected %d at index %d, but got %d", v, i, result[i])
+		}
+	}
+}
