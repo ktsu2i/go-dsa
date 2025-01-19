0c8ff2adee241278fce3f15c3214edec8933a7cc
ktsu2i <kaito.tsu2i@gmail.com>
2025-01-19 17:36:26 +0900

    test for Add()

---
+++ sets/set/set_test.go
@@ -0,0 +1,28 @@
+package set_test
+
+import (
+	"go-dsa/sets/set"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	s := set.New[int]()
+
+	// 1. Add one value
+	s.Add(1)
+	if s.Size() != 1 {
+		t.Errorf("Expected size 1, but got %d", s.Size())
+	}
+	if !s.Contains(1) {
+		t.Errorf("Expected to have 1")
+	}
+
+	// 2. Add two values
+	s.Add(2)
+	if s.Size() != 2 {
+		t.Errorf("Expected size 1, but got %d", s.Size())
+	}
+	if !s.Contains(2) {
+		t.Errorf("Expected to have 2")
+	}
+}
