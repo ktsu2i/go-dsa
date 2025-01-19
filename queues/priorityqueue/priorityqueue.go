818e959cd2b11bafcbd1b75381ee09576d240edb
ktsu2i <kaito.tsu2i@gmail.com>
2025-01-19 17:01:44 +0900

    created Priority Queue

---
+++ queues/priorityqueue/priorityqueue.go
@@ -0,0 +1,35 @@
+package priorityqueue
+
+import (
+	"go-dsa/trees/minheap"
+
+	"golang.org/x/exp/constraints"
+)
+
+type PriorityQueue[T constraints.Ordered] struct {
+	heap *minheap.MinHeap[T]
+}
+
+func New[T constraints.Ordered]() *PriorityQueue[T] {
+	return &PriorityQueue[T]{heap: minheap.New[T]()}
+}
+
+func (pq *PriorityQueue[T]) Enqueue(value T) {
+	pq.heap.Push(value)
+}
+
+func (pq *PriorityQueue[T]) Dequeue() (T, bool) {
+	return pq.heap.Pop()
+}
+
+func (pq *PriorityQueue[T]) Peek() (T, bool) {
+	return pq.heap.Peek()
+}
+
+func (pq *PriorityQueue[T]) Size() int {
+	return pq.heap.Size()
+}
+
+func (pq *PriorityQueue[T]) IsEmpty() bool {
+	return pq.heap.IsEmpty()
+}
