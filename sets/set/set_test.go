fa0784cb7581c3ebae652b6a33a6ad29a599043c
ktsu2i <kaito.tsu2i@gmail.com>
2025-01-19 17:39:19 +0900

    updated test for Add()

--- sets/set/set_test.go
+++ sets/set/set_test.go
@@ -20,9 +20,18 @@ func TestAdd(t *testing.T) {
 	// 2. Add two values
 	s.Add(2)
 	if s.Size() != 2 {
-		t.Errorf("Expected size 1, but got %d", s.Size())
+		t.Errorf("Expected size 2, but got %d", s.Size())
 	}
 	if !s.Contains(2) {
 		t.Errorf("Expected to have 2")
+	}
+
+	// 3. Add same number
+	s.Add(1)
+	if s.Size() != 2 {
+		t.Errorf("Expected size 2, but got %d", s.Size())
+	}
+	if !s.Contains(1) {
+		t.Errorf("Expected to have 1")
 	}
 }
