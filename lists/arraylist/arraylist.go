e32e3e6cad15401e0aba5813112bd9fd8002b2ba
ktsu2i <kaito.tsu2i@gmail.com>
2025-01-20 23:01:04 +0900

    merge sort

--- lists/arraylist/arraylist.go
+++ lists/arraylist/arraylist.go
@@ -1,10 +1,12 @@
 package arraylist
 
-type ArrayList[T comparable] struct {
+import "golang.org/x/exp/constraints"
+
+type ArrayList[T constraints.Ordered] struct {
 	values []T
 }
 
-func New[T comparable]() *ArrayList[T] {
+func New[T constraints.Ordered]() *ArrayList[T] {
 	return &ArrayList[T]{
 		values: make([]T, 0),
 	}
@@ -41,4 +43,41 @@ func (al *ArrayList[T]) Contains(value T) bool {
 		}
 	}
 	return false
+}
+
+func (al *ArrayList[T]) MergeSort() {
+	al.mergeSort(al.values)
+}
+
+// Helper functions
+
+func (al *ArrayList[T]) mergeSort(arr []T) []T {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	left := al.mergeSort(arr[:mid])
+	right := al.mergeSort(arr[mid:])
+
+	return al.merge(left, right)
+}
+
+func (al *ArrayList[T]) merge(left, right []T) []T {
+	result := make([]T, 0, len(left)+len(right))
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return result
 }
