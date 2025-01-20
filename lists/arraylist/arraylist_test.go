e92c553b76d776d66ee7e9cdffb8870fca3764a5
ktsu2i <kaito.tsu2i@gmail.com>
2025-01-20 22:20:52 +0900

    test for Remove()

--- lists/arraylist/arraylist_test.go
+++ lists/arraylist/arraylist_test.go
@@ -34,3 +34,31 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Expected 20, but got %d", val)
 	}
 }
+
+func TestRemove(t *testing.T) {
+	al := arraylist.New[int]()
+
+	// 1. Empty
+	ok := al.Remove(100)
+	if ok {
+		t.Errorf("Expected ok=false, but ok=true")
+	}
+
+	// 2. Size 2
+	al.Add(10)
+	al.Add(20)
+	ok = al.Remove(10)
+	if !ok {
+		t.Errorf("Expected ok=true, but got false")
+	}
+	if al.Size() != 1 {
+		t.Errorf("Expected size 1, but got %d", al.Size())
+	}
+	val, ok := al.Get(0)
+	if !ok {
+		t.Errorf("expected ok=true, but got false")
+	}
+	if val != 20 {
+		t.Errorf("Expected 20, but got %d", val)
+	}
+}
