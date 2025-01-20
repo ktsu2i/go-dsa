f1a4b11647e8f40433e4615867f34837946e6846
ktsu2i <kaito.tsu2i@gmail.com>
2025-01-20 22:17:11 +0900

    test for Add()

---
+++ lists/arraylist/arraylist_test.go
@@ -0,0 +1,36 @@
+package arraylist_test
+
+import (
+	"go-dsa/lists/arraylist"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	al := arraylist.New[int]()
+
+	// 1. Empty
+	if al.Size() != 0 {
+		t.Errorf("Expected size 0, but got %d", al.Size())
+	}
+
+	// 2. Size 2
+	al.Add(10)
+	al.Add(20)
+	if al.Size() != 2 {
+		t.Errorf("Expected size 2, but got %d", al.Size())
+	}
+	val, ok := al.Get(0)
+	if !ok {
+		t.Errorf("Expected ok=true, but got false")
+	}
+	if val != 10 {
+		t.Errorf("Expected 10, but got %d", val)
+	}
+	val, ok = al.Get(1)
+	if !ok {
+		t.Errorf("Expected ok=true, but got false")
+	}
+	if val != 20 {
+		t.Errorf("Expected 20, but got %d", val)
+	}
+}
