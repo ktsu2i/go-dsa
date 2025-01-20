ce42afd81364a32277fc344816401315c6cb7d66
ktsu2i <kaito.tsu2i@gmail.com>
2025-01-20 21:30:07 +0900

    created New() and AddVertex()

---
+++ graph/graph.go
@@ -0,0 +1,31 @@
+package graph
+
+import "sync"
+
+type vertex[T comparable] struct {
+	id    T
+	value any
+}
+
+type edge[T comparable] struct {
+	from   T
+	to     T
+	weight float64
+}
+
+type Graph[T comparable] struct {
+	vertices map[T]*vertex[T]
+	edges    map[T][]edge[T]
+	directed bool
+	weighted bool
+	lock     sync.RWMutex
+}
+
+func New[T comparable](directed bool, weighted bool) *Graph[T] {
+	return &Graph[T]{
+		vertices: make(map[T]*vertex[T]),
+		edges:    make(map[T][]edge[T]),
+		directed: directed,
+		weighted: weighted,
+	}
+}
